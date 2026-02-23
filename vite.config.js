5e9dd8fd0450ba3bd5e8049ccdcb69a0be5a83a9
Coders Club <hi@codersclub.co>
2026-02-24 00:50:08 +0300

    İllüstrasyon detay görselleri eklendi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // GitHub Pages alt dizinde çalışması için (yoncaisyar.github.io/REPO_ADI/)
   plugins: [react()],
   optimizeDeps: {
     include: ['i18next', 'react-i18next']
